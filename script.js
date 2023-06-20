cfdeeb63a7ac2f9c42cc11e6aad0b4f34d4792fb
Eliel Souza <eliellsouzza@gmail.com>
2023-06-20 09:02:14 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,32 @@
+class Produto{
+    constructor() {
+        this.id = 0;
+        this.name = "";
+        this.valor = 0;
+    }
+
+    adicionar() {
+
+
+        alert('vamos adicionar um produto');
+
+        
+    }    
+
+
+    excluir() {
+
+
+
+        alert('item deletado')
+        
+
+
+    }
+}
+
+const produto = new Produto()
+
+
+
+ 
